6867995faffb42c2f62ad06d9ebd4bfec5de699b
github username <gregm9999@gmail.com>
2019-06-06 17:54:37 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -24,13 +24,20 @@ const realForm = document.querySelector(".add-toy-form");
 realForm.addEventListener("submit", postCall);
 document.addEventListener("DOMContentLoaded", function() {
   getCall();
-  addEventListener();
+  // addEventListener();
 });
 
 function getCall() {
   fetch(toyUrl)
     .then(result => result.json())
-    .then(toy => renderToys(toy));
+    .then(toy => renderToys(toy))
+    .catch(err => console.log(err));
+}
+
+function renderToys(toyAr) {
+  for (let i = 0; i < toyAr.length; i++) {
+    renderToy(toyAr[i]);
+  }
 }
 
 function renderToy(toy) {
@@ -52,42 +59,16 @@ function renderToy(toy) {
 
   pID.textContent = "ID: " + toy.id;
   h2.textContent = toy.name;
-
   img.src = toy.image;
   img.classList.add("toy-avatar");
-
   pLikes.textContent = toy.likes + " Likes";
   button.textContent = "Like <3";
-
-  console.log("buttons?");
   button.addEventListener("click", () => addLike(toy, pLikes));
-
   toyCollection.appendChild(div);
 }
 
-function renderToys(toyAr) {
-  let toyCollection = document.getElementById("toy-collection");
-  for (let i = 0; i < toyAr.length; i++) {
-    renderToy(toyAr[i]);
-    //   div = `<div class="card">
-    //   <p>ID: ${toyAr[i].id}</p>
-    //   <h2>${toyAr[i].name}</h2>
-    //   <p>${toyAr[i].likes} Likes</p>
-    //   <img src="${toyAr[i].image}" class="toy-avatar" />
-    //   <button class="like-btn">Like <3</button>
-    // </div>`;
-    // toyCollection.innerHTML += div;
-  }
-}
-
-function addEventListener() {
-  submitBtn.addEventListener("submit", event => {
-    event.preventDefault();
-    postCall(event.target.elements);
-  });
-}
-
 function postCall(event) {
+  console.log("postCall fires");
   event.preventDefault();
   let inputs = document.querySelectorAll(".input-text");
   let name = inputs[0].value;
@@ -100,11 +81,12 @@ function postCall(event) {
     body: JSON.stringify({ name: name, image: image, likes: 0 })
   })
     .then(response => {
-      response.json();
+      return response.json();
     })
     .then(toy => {
-      renderToy(toy);
-    });
+      return renderToys([toy]);
+    })
+    .catch(err => console.log(err));
 }
 
 function addLike(toy, likesEl) {
@@ -114,7 +96,6 @@ function addLike(toy, likesEl) {
 
   console.log("likes=", likes);
   console.log("id=", id);
-  console.log("addLike fires");
 
   fetch(toyUrl + "/" + id, {
     method: "PATCH",
@@ -129,5 +110,7 @@ function addLike(toy, likesEl) {
     })
     .then(toy => {
       likesEl.innerText = `${likes} likes`;
-    });
+      toy.likes++;
+    })
+    .catch(err => console.log(err));
 }
